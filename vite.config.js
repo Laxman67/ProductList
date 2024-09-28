c1017b80cea44753448e386d5b2edd2bd2891804
laxman67 <laxmanhenricks94@gmail.com>
2024-09-29 02:06:17 +0530

    Done

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': __dirname + '/src',
+      '@': __dirname + '/src', // Ensure this alias points to the correct path
     },
   },
 });
